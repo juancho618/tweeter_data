12020eb764400691d3a044b4349bebac097d3edd
juan <jjsorianoe@gmail.com>
2017-12-26 14:00:03 +0100

    tweet formated to the db and tweet cleaning

--- python/StreamTwitterData.py
+++ python/StreamTwitterData.py
@@ -9,6 +9,7 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener, json
 from pymongo import MongoClient
 from nltk.tokenize import TweetTokenizer
+from clean import cleanTweet
 
 
 consumerkey="cnV6zLTpFswKMIgpgvTfbae3g"
@@ -23,19 +24,26 @@ class listener(StreamListener):
         try:
             client = MongoClient('localhost',27017)
             # twitterdb is the new db that I created to store the tweets
-            db = client.twitterdatabase
+            db = client.clean_tweets
 
+            
             # Decode the JSON from Twitter
             datajson = json.loads(data)
+            #Get just the tweet
+            tweet = datajson['text']
+            # apply the cleaning function to the tweet
+            clean_tweet = cleanTweet(tweet)
+            # Python Object that would be save in the db.
+            pyObject = { 'text': clean_tweet, 'fullResponse': datajson }  
 
             #This is optional, I saw it as a suggestion from a website.
             #grab the 'created_at' data from the Tweet to use for display
             created_at = datajson['created_at']
             #print out a message to the screen that we have collected a tweet
-            print("Tweet collected at " + str(created_at))
+            #print("Tweet collected at " + str(created_at))
 
             #It will insert into 'tweets' collection the data that are streamed
-            db.tweets.insert(datajson)
+            db.tweets.insert(pyObject)
         except Exception as e:
            print(e)
 
@@ -45,5 +53,5 @@ class listener(StreamListener):
 auth=OAuthHandler(consumerkey,consumersecret)
 auth.set_access_token(accesskey,accesssecret)
 twitterStream=Stream(auth,listener())
-twitterStream.filter(track=['science'], locations=[-122.995004, 32.323198,-67.799695, 49.893813])
+twitterStream.filter(locations=[-122.995004, 32.323198,-67.799695, 49.893813])
 
---
+++ python/clean.py
@@ -0,0 +1,35 @@
+import re
+import nltk
+import numpy
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+tknz = TweetTokenizer()
+clean_tweets = []
+url_expression = 'http[s]?[:]?//(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+def cleanTweet(tweet):    
+    wordnet_lemmatizer = WordNetLemmatizer()
+    stop = set(stopwords.words('english')) #stop words!
+    framents = tknz.tokenize(tweet)
+    clean_fragments = []
+    for f in framents:
+        if f not in stop: # not included in the stop words
+            f = f.lower() #lowercase fragment
+            f = re.sub(r'[.,"!~_:|?]+', '', f,flags=re.MULTILINE) # Special characters
+            f = re.sub(url_expression, '', f,flags=re.MULTILINE) # links
+            f = re.sub(r'@[a-z,A-Z,0-9 ]*', '', f, flags=re.MULTILINE) #clean at person references
+            f = re.sub(r'RT @[a-z,A-Z]*: ', '', f, flags=re.MULTILINE) #Remove retweets
+            f = wordnet_lemmatizer.lemmatize(f)
+            if f:
+                clean_fragments.append(f)
+    return(" ".join(clean_fragments))
+
+    
+        
+
+
+
+# links :
+# https://stackoverflow.com/questions/26304191/get-the-document-name-in-scikit-learn-tf-idf-matrix
+# https://www.youtube.com/watch?v=RPMYV-eb6lI
