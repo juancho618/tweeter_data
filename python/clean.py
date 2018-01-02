6ae355adcda7e0cc7f5ef4f55d8265452a9c928f
Redona <RedonaBrahimetaj>
2018-01-02 01:32:16 +0100

    Report almost done

--- python/clean.py
+++ python/clean.py
@@ -12,7 +12,7 @@ tknz = TweetTokenizer()
 clean_tweets = []
 
 #we define the fortmat of the url so later on we can exclude it from the tweets
-url_expression = 'http[s]?[:]?//(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+url_expression = "http[s]?[:]?//(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 
 #function to clean tweet
 def cleanTweet(tweet):    
